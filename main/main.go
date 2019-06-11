a7d58ec63c90175a537f185e6ab24cc11e1a35e8
K <kamiyaowl@gmail.com>
2019-06-12 01:06:55 +0900

    とりあえず作成

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+// This is the most minimal blinky example and should run almost everywhere.
+
+import (
+	"machine"
+	"time"
+)
+
+func main() {
+	led := machine.GPIO{machine.LED}
+	led.Configure(machine.GPIOConfig{Mode: machine.GPIO_OUTPUT})
+	for {
+		led.Low()
+		time.Sleep(time.Millisecond * 300)
+
+		led.High()
+		time.Sleep(time.Millisecond * 300)
+	}
+}
